f25fc180f3cf0c9ba24801fe1021bcd42848936f
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-11 14:54:03 -0500

    Removed commented out plugin

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ module.exports = {
     },
     variants: {},
     plugins: [
-        // require('rds-tailwind-theme'),
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography')
     ],
